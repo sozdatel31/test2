eea7193404b88910d5c27369d2fd1933d27ab0a1
sozdatel31 <pavlik.gerasim@gmail.com>
2021-02-22 23:46:06 +0300

    аккордион тайтл и аккордион бади созданы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,74 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      This is App component
-    </div>
-  );
+function hello() {
+    debugger
+    alert("hello IT-camasutra");
 }
+
+//hello();
+
+function App() {
+    console.log("App rendered");
+    debugger
+    return (
+        <div>
+            <Rating/>
+            <Accordion/>
+            <AppTitle/>
+            <Rating/>
+        </div>
+    );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendered");
+    return (<>This is App component</>);
+}
+
+function Rating() {
+    console.log("Rating rendered");
+    return (<div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    console.log("Star rendered");
+    return (<div>
+            Star
+        </div>
+
+    );
+}
+
+function Accordion() {
+    console.log("Accordeon rendered");
+    return (<div>
+            <AccordionTitle/>
+            <AccordionBody/>
+
+
+        </div>
+
+    );
+
+}
+
+function AccordionTitle() {
+    return <h3>Меню</h3>
+};
+function AccordionBody() {
+return  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+};
 
 export default App;
